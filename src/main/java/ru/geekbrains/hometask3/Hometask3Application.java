b0b9b46bb30af0489646468955e22f240bf1d9fa
Vadim <119964682+RustyFrukt@users.noreply.github.com>
2024-03-21 14:23:39 +0300

    DZ

---
+++ src/main/java/ru/geekbrains/hometask3/Hometask3Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.hometask3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hometask3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hometask3Application.class, args);
+	}
+
+}
